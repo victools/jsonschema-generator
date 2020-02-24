d1848d6132b68e0eff87212271388f98c7e7df8e
Carsten Wickner <11309681+CarstenWickner@users.noreply.github.com>
2020-02-24 22:25:26 +0100

    chore: revert unnecessary change

--- src/test/java/com/github/victools/jsonschema/generator/impl/SchemaGeneratorConfigImplTest.java
+++ src/test/java/com/github/victools/jsonschema/generator/impl/SchemaGeneratorConfigImplTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.github.victools.jsonschema.generator.impl;
 
 import com.fasterxml.classmate.ResolvedType;
