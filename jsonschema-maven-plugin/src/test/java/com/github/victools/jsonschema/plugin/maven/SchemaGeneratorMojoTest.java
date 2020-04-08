768d786d6793743423295e8de51ee8b75047a92d
jlabrie <jan.labrie@asml.com>
2020-04-08 13:19:47 +0200

    First version of the maven plugin for the JSON schema generator

---
+++ jsonschema-maven-plugin/src/test/java/com/github/victools/jsonschema/plugin/maven/SchemaGeneratorMojoTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2020 VicTools.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.victools.jsonschema.plugin.maven;
+
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.configuration.PlexusConfiguration;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
+
+import java.io.File;
+import java.io.StringReader;
+
+public class SchemaGeneratorMojoTest extends AbstractMojoTestCase {
+
+    private PlexusConfiguration pluginConfiguration;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void setUp()
+            throws Exception {
+        super.setUp();
+
+        String pom =
+                "<project>\n" +
+                        "    <build>\n" +
+                        "        <plugins>\n" +
+                        "            <plugin>\n" +
+                        "                <groupId>com.github.victools</groupId>\n" +
+                        "                <artifactId>jsonschema-maven-plugin</artifactId>\n" +
+                        "                <version>4.8.0</version>\n" +
+                        "                <configuration>\n" +
+                        "                    <className>" +
+                        "com.github.victools.jsonschema.plugin.maven.TestClass</className>\n" +
+                        "                    <schemaFileName>result.json</schemaFileName>\n" +
+                        "                    <schemaFilePath>src/test/resources</schemaFilePath>\n" +
+                        "                    <options>\n" +
+                        "                        <option>DEFINITIONS_FOR_ALL_OBJECTS</option>\n" +
+                        "                    </options>\n" +
+                        "                    <modules>\n" +
+                        "                        <module>\n" +
+                        "                            <name>Jackson</name>\n" +
+                        "                            <options>\n" +
+                        "                                <option>FLATTENED_ENUMS_FROM_JSONVALUE</option>\n" +
+                        "                            </options>\n" +
+                        "                        </module>\n" +
+                        "                        <module>\n" +
+                        "                            <name>MyModule</name>\n" +
+                        "                            <className>" +
+                        "com.github.victools.jsonschema.plugin.maven.TestModule</className>\n" +
+                        "                        </module>\n" +
+                        "                    </modules>\n" +
+                        "                </configuration>\n" +
+                        "            </plugin>\n" +
+                        "        </plugins>\n" +
+                        "    </build>" +
+                        "</project>";
+
+        Xpp3Dom pomDom = Xpp3DomBuilder.build(new StringReader(pom));
+        pluginConfiguration = extractPluginConfiguration("jsonschema-maven-plugin", pomDom);
+    }
+
+    /**
+     * Unit test to test the basic working with a complete configuration
+     *
+     * @throws Exception In case something goes wrong
+     */
+    public void testBasicWorking() throws Exception {
+        SchemaGeneratorMojo myMojo = (SchemaGeneratorMojo) lookupConfiguredMojo(new MavenProject(), "schema-generator");
+        myMojo = (SchemaGeneratorMojo) configureMojo(myMojo, pluginConfiguration);
+
+        myMojo.execute();
+
+        // Validate that the schema files is created.
+        File file = new File("src/test/resources/result.json");
+        assertTrue(file.exists());
+    }
+}
