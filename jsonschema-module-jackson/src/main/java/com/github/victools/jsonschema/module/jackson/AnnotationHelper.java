d559d8c11def38a14f39cd34a1cba3dfcf9516a2
Smaarn <antoine.malliarakis@gmail.com>
2024-11-01 11:00:57 +0100

    PR feedback: extract non "Jackson specific" logic into shared class in the base package

--- jsonschema-module-jackson/src/main/java/com/github/victools/jsonschema/module/jackson/AnnotationHelper.java
+++ jsonschema-module-jackson/src/main/java/com/github/victools/jsonschema/module/jackson/AnnotationHelper.java
@@ -20,13 +20,9 @@ import com.fasterxml.classmate.members.ResolvedMember;
 import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedElement;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 final class AnnotationHelper {
 
@@ -50,11 +46,11 @@ final class AnnotationHelper {
      * @return an empty entry if not found
      */
     static <A extends Annotation> Optional<A> resolveAnnotation(ResolvedMember<?> member, Class<A> annotationClass) {
-        final A annotation = member.getAnnotations().get(annotationClass);
-        if (annotation != null) {
-            return Optional.of(annotation);
-        }
-        return resolveNestedAnnotations(StreamSupport.stream(member.getAnnotations().spliterator(), false), annotationClass);
+        return com.github.victools.jsonschema.generator.AnnotationHelper.resolveAnnotation(
+                member,
+                annotationClass,
+                JACKSON_ANNOTATIONS_INSIDE_ANNOTATED_FILTER
+        );
     }
 
     /**
@@ -70,28 +66,11 @@ final class AnnotationHelper {
      * @return an empty entry if not found
      */
     static <A extends Annotation> Optional<A> resolveAnnotation(AnnotatedElement declaringType, Class<A> annotationClass) {
-        final A annotation = declaringType.getAnnotation(annotationClass);
-        if (annotation != null) {
-            return Optional.of(annotation);
-        }
-        return resolveNestedAnnotations(Arrays.stream(declaringType.getAnnotations()), annotationClass);
+        return com.github.victools.jsonschema.generator.AnnotationHelper.resolveAnnotation(
+                declaringType,
+                annotationClass,
+                JACKSON_ANNOTATIONS_INSIDE_ANNOTATED_FILTER
+        );
     }
-    
-    private static <A extends Annotation> Optional<A> resolveNestedAnnotations(Stream<Annotation> initialAnnotations, Class<A> annotationClass) {
-        List<Annotation> annotations = extractNestedAnnotations(initialAnnotations);
-        while (!annotations.isEmpty()) {
-            final Optional<Annotation> directAnnotation = annotations.stream().filter(annotationClass::isInstance).findFirst();
-            if (directAnnotation.isPresent()) {
-                return directAnnotation.map(annotationClass::cast);
-            }
-            annotations = extractNestedAnnotations(annotations.stream());
-        }
-        return Optional.empty();
-    }
-    
-    private static List<Annotation> extractNestedAnnotations(Stream<Annotation> annotations) {
-        return annotations.filter(JACKSON_ANNOTATIONS_INSIDE_ANNOTATED_FILTER)
-                .flatMap(a -> Arrays.stream(a.annotationType().getAnnotations()))
-                .collect(Collectors.toList());
-    }
+
 }
